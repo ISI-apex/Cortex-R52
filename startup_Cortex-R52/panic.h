7819f6e11ae71b14a13cbae1ee90807e858439cd
Alexei Colin <acolin@isi.edu>
2018-11-01 11:24:35 -0400

    panic: add assert

--- startup_Cortex-R52/panic.h
+++ startup_Cortex-R52/panic.h
@@ -1,6 +1,10 @@
 #ifndef PANIC_H
 #define PANIC_H
 
+#include "printf.h"
+
+#define ASSERT(cond) if (!(cond)) { panic("ASSERT: " #cond "\r\n");  }
+
 void panic(const char *msg);
 
 #endif // PANIC_H
